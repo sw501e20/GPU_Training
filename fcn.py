53b7ecac00250a8b5fb7e492c464ab2f55be5455
sw501e20 <74351339+sw501e20@users.noreply.github.com>
2020-11-18 11:19:55 +0100

    Model trained

--- fcn.py
+++ fcn.py
@@ -26,7 +26,7 @@ print(f'\n\nShapes: x_train = {x_train.shape}, y_train = {y_train.shape}, '
 input_shape = x_train.shape[1:]
 num_classes = y_train.shape[1]
 batch_size = 128 
-num_epochs = 2000
+num_epochs = 800
 
 
 # Define model:
